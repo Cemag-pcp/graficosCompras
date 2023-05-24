138de7e72312ac26e6ad430882bcd4bece5cc774
luan <luan640@gmail.com>
2023-05-24 16:21:09 -0300

    s

--- mainv3.py
+++ mainv3.py
@@ -12,7 +12,7 @@ import numpy as np
 
 warnings.filterwarnings("ignore")
 
-@st.cache_data(allow_output_mutation=True)
+@st.cache_data()
 def load_sheets():
 
     filename = 'service_account.json'
@@ -92,7 +92,7 @@ def load_sheets():
 
     return dfSimulacao, dfDatas, dfPedidos
 
-@st.cache_data(allow_output_mutation=True)
+@st.cache_data()
 def tratamento():
 
     hoje = datetime.now()
